3f0faa87ff393f49888a2c74957ba85aed0c60d2
fer <ferp@protonmail.ch>
2020-05-06 10:18:43 +0200

    creating adapters for endpoints

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const handleUserRequest = require('./controllers/user-endpoint')
+const handleFriendRequest = require('./controllers/friend-endpoint')
+const adaptRequest = require('./helpers/adaptRequest')
 
 const server = express()
 server.use(bodyParser.json())
+
+function usersController (req, res) {
+  const httpRequest = adaptRequest(req)
+  handleUserRequest(httpRequest)
+    .then(({ headers, statusCode, data = {} }) => {
+      res
+        .set(headers)
+        .status(statusCode)
+        .send(data)
+    })
+    .catch(err => res.status(500).end())
+}
+
+function friendsController (req, res) {
+  const httpRequest = adaptRequest(req)
+  handleFriendRequest(httpRequest)
+    .then(({ headers, statusCode, data = {} }) => {
+      res
+        .set(headers)
+        .status(statusCode)
+        .send(data)
+    })
+    .catch(err => res.status(500).end())
+}
 
 server.listen(9090, () => console.log('listening on port 9090'))
 
