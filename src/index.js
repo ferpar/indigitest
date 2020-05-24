cf0634959275450aa2bb0781bcac1174d51eb42a
fer <ferp@protonmail.ch>
2020-05-24 10:43:26 +0200

    refactored callback adapter @ main index.js

--- src/index.js
+++ src/index.js
@@ -3,46 +3,38 @@ const bodyParser = require('body-parser')
 const adaptRequest = require('./helpers/adaptRequest')
 
 const { handleUserRequest, handleFriendRequest } = require('./controllers')
+const usersEndpoint = makeEndpointCallback(handleUserRequest);
+const friendsEndpoint = makeEndpointCallback(handleFriendRequest);
+
 
 const server = express()
 server.use(bodyParser.json())
 
-server.all('/user', usersController)
-server.get('/user/:id', usersController)
-server.delete('/user/:id', usersController)
+server.all('/user', usersEndpoint)
+server.get('/user/:id', usersEndpoint)
+server.delete('/user/:id', usersEndpoint)
 
-server.all('/friend', friendsController)
-server.get('/friend/:type/:id', friendsController)
+server.all('/friend', friendsEndpoint)
+server.get('/friend/:type/:id', friendsEndpoint)
 
-function usersController (req, res) {
-  console.log('httpRequest: ' + req.method + ' ' + req.path  )
-  const httpRequest = adaptRequest(req)
-  handleUserRequest(httpRequest)
-    .then(({ headers, statusCode, data = {} }) => {
-      console.log(statusCode)
-      console.log(data)
-      res
-        .set(headers)
-        .status(statusCode)
-        .send(data)
-    })
-    .catch(err => res.status(500).end())
+
+function makeEndpointCallback (controller) {
+  return function (req, res) {
+    console.log('httpRequest: ' + req.method + ' ' + req.path  )
+    const httpRequest = adaptRequest(req)
+    controller(httpRequest)
+      .then(({ headers, statusCode, data = {} }) => {
+        console.log(statusCode)
+        console.log(data)
+        res
+          .set(headers)
+          .status(statusCode)
+          .send(data)
+      })
+      .catch(err => res.status(500).end())
+  }
 }
 
-function friendsController (req, res) {
-  console.log('httpRequest: ' + req.method + ' ' + req.path  )
-  const httpRequest = adaptRequest(req)
-  handleFriendRequest(httpRequest)
-    .then(({ headers, statusCode, data = {} }) => {
-      console.log(statusCode)
-      console.log(data)
-      res
-        .set(headers)
-        .status(statusCode)
-        .send(data)
-    })
-    .catch(err => res.status(500).end())
-}
 
 server.listen(9090, () => console.log('listening on port 9090'))
 
