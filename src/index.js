5f132520bee2f956c9163c174456df306c1421ae
fer <ferp@protonmail.ch>
2020-06-09 23:21:28 +0200

    removed unnecessary code

--- src/index.js
+++ src/index.js
@@ -4,10 +4,4 @@ const app = require('./app')
 
 const server = app.listen( process.env.PORT || 9090, () => console.log(`listening on port ${process.env.PORT || 9090}`))
 
-process.on('SIGTERM', () => {
-  server.close(() => {
-    console.log('Process terminated')
-  })
-})
-
 module.exports = server
