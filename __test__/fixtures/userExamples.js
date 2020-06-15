163feb126009115e372343895884492967a67b96
fer <ferp@protonmail.ch>
2020-06-15 19:03:56 +0200

    added some example fixtures

---
+++ __test__/fixtures/userExamples.js
@@ -0,0 +1,45 @@
+module.exports = {
+  user1: 
+  { 
+    id: "ck9vik2sw0000cytlchrg5kik",
+    username: "Fran.Dietrch27",
+    email: "Cale30@hotail.cm",
+    password: "lFgE9Cus7maKtpr",
+    longitude: "27.3772", 
+    latitude: "81.1052",
+    browserlang: "es"
+  },
+
+  user2:
+  { 
+    id: "ck9vik2sw0000cytlchrg5cac",
+    username: "Paco.Pervez27",
+    email: "Cale25@hotail.cm",
+    password: "s7maKtprlFgE9Cu",
+    longitude: "34.3772", 
+    latitude: "15.1052",
+    browserlang: "es"
+  },
+
+  user3:
+  { 
+    id: "ck9vik2sw0000cytlchrg5woo",
+    username: "Woohoo",
+    email: "Cal5@hotail.cm",
+    password: "s7maKtE9CuprlFg",
+    longitude: "43.3772", 
+    latitude: "28.1052",
+    browserlang: "es"
+  },
+
+  user4:
+  { 
+    id: "ck9vik2sw0000cytlchrg5kke",
+    username: "Fran.burkur27",
+    email: "Cale28@tmail.cm",
+    password: "lFgE9Cus7maKtpr",
+    longitude: "27.3772", 
+    latitude: "81.1052",
+    browserlang: "es"
+  }
+}
