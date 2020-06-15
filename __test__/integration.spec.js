bb68148f9ca2f6899a81d4fd7f917f7123cdfe62
fer <ferp@protonmail.ch>
2020-06-15 19:02:46 +0200

    extended integration tests

--- __test__/integration.spec.js
+++ __test__/integration.spec.js
@@ -2,30 +2,105 @@ const request = require('supertest')
 const server = require('../src')
 const db = require('../src/db')
 
-//schema examples
-const user1Input = {
-        id: "ck9vik2sw0000cytlchrg5kik",
-        username: "testyMctestFace",
-        email: "Cale30@hotail.cm",
-        password: "lFgE9Cus7maKtpr",
-        longitude: 27.3772,
-        latitude: 81.1052,
-        browserlang: "es"
-      }
+const makeUserDb = require('../src/db/userdb/userdb')
+const userDb = makeUserDb({ db })
+const makeFakeUser = require('./fixtures/user')
+const makeUser = require('../src/domain/user-model')
+
+//----------pretest---------------
+//this section 
+//1) autogenerates random users for the test
+//2) allows to reset the db state after each test
+
+//this query clears all tables due to cascading references
+const cleanupDbQuery = "DELETE FROM users;"
+
+//generating user examples
+const userInfoExamples = []
+for ( let i = 5 ; i-- ; i > 0) {
+  userInfoExamples.push(makeFakeUser({}, {
+      hemisphere: 'North', 
+      flatten: true
+    })
+  )
+}
+const userObjects = userInfoExamples
+  .slice(1)
+  .map( userInfo => makeUser(userInfo))
+
+//function to use beforeEach test
+async function resetInitialState() {
+  //clean up DB
+  await db.query(cleanupDbQuery);
+  //insert Users
+  for ( let userObj of userObjects ) {
+   await userDb.insert(userObj)
+  }
+  //create friendships between user[1] and rest of users except user[0]
+  for (let userObj of userObjects.slice(1) ) {
+    await userDb.addFriendship(
+      userObjects[0].getId(),
+      userObj.getId()
+    )
+  }
+}
+//--------------------------------
 
 //tests
-describe('Post User Endpoint', () => {
+describe('Indigitest API', () => {
   afterAll( async () => {
-    await request(server).delete(`/user/${user1Input.id}`)
-    db.shutdown()
-    server.close()
+    await db.shutdown();
+    server.close();
   })
-  it('should create a new user', async () => {
-    const resp = await request(server)
-      .post('/user')
-      .send(user1Input)
-    expect(resp.statusCode).toEqual(201)
-  })  
+  describe('/user resource', () => {
+    beforeEach( async () => {
+      await resetInitialState()
+    })
+    afterEach( async () => {
+      await db.query(cleanupDbQuery) 
+    })
+    describe('POST Method', () => {
+      it('stores new users', async () => {
+        const resp = await request(server)
+          .post('/user')
+          .send(userInfoExamples[0]) //inserts user[0]
+        expect(resp.statusCode).toEqual(201)
+      }) 
+    })
+    describe('GET /user/{userId}', () => {
+      it('finds users', async () => {
+        expect.assertions(2)
+        const resp = await request(server)
+          .get(`/user/${userObjects[0].getId()}`)
+        expect(resp.body.id).toEqual(userObjects[0].getId())
+        expect(resp.statusCode).toEqual(200)
+      })
+    })
+    describe('PUT /user/{userId}', () => {
+      it('updates users', async () => {
+        expect.assertions(2)
+        const user0Infomodified = { 
+          ...userInfoExamples[1],
+          username:"TestyMcTestFace"
+        }
+        delete user0Infomodified.id
+        const resp = await request(server)
+          .put(`/user/${userInfoExamples[1].id}`)
+          .send(user0Infomodified)
+        expect(resp.body.username).toEqual("TestyMcTestFace")
+        expect(resp.statusCode).toEqual(200)
+      })
+    })
+    describe('DELETE /user/{userId}', () => {
+      it('removes users', async () => {
+        const resp = await request(server)
+          .delete(`/user/${userObjects[2].getId()}`)
+        expect(resp.body.removeCount).toEqual(1)
+        expect(resp.statusCode).toEqual(200)
+      })
+    })
+  })
+  describe('/friend resource', () => {})
 })
 
 
